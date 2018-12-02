920edc707e9448081ba07a41aed7a3094f5c7ca6
hjy <yuelove110@163.com>
2018-12-02 19:51:44 +0800

    Site updated: 2018-12-02 19:51:35

--- js/src/motion.js
+++ js/src/motion.js
@@ -342,11 +342,13 @@ $(document).ready(function () {
     },
 
     sidebar: function (integrator) {
+      if (CONFIG.sidebar === 'always') {
       if (CONFIG.sidebar.display === 'always') {
         NexT.utils.displaySidebar();
       }
       integrator.next();
     }
+  }
   };
 
 });
